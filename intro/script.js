ba19ea59a2ef6bdea9a2fccea53baec358bba9cc
Helena <helena@klarr.se>
2025-04-03 08:37:11 +0200

    init

---
+++ intro/script.js
@@ -0,0 +1,18 @@
+// TASK MANAGER APP
+
+// vi ska skapa en liten app där vi kan lägga till "tasks", markera en task som klar,
+// visa alla tasks samt filtrera ut alla klara tasks.
+// användaren gör val i en meny och vår app visar sedan korrekt val
+
+// vår app ska bestå av två delar:
+// 1. ett objekt som ska innehålla all logik
+// 2. en funktion som ska hantera menyval
+
+// vårt objekt ska ha följande properties:
+// - array som innehåller alla tasks
+// - funktion för att lägga till en ny task
+// - funktion för att markera en task som klar
+// - funktion för att visa alla tasks
+// - funktion för att visa alla tasks som är klara
+
+// vår menu() ska ta emot en input från en user och visa rätt val
